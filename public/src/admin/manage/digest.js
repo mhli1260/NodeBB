18adcf3c2cb0c4103e6f2dd389cdc8c0a8a7fa28
mhli1260 <118139796+mhli1260@users.noreply.github.com>
2024-09-03 22:22:04 -0400

    Adding errorCheck function and call to reduce nesting

--- public/src/admin/manage/digest.js
+++ public/src/admin/manage/digest.js
@@ -17,13 +17,7 @@ define('admin/manage/digest', ['bootbox', 'alerts'], function (bootbox, alerts) 
 				const interval = action.slice(7);
 				bootbox.confirm('[[admin/manage/digest:resend-all-confirm]]', function (ok) {
 					if (ok) {
-						Digest.send(action, undefined, function (err) {
-
-							if (err) {
-								return alerts.error(err);
-							}
-							alerts.success('[[admin/manage/digest:resent-' + interval + ']]');
-						});
+						Digest.send(action, undefined, errorCheck(err));
 					}
 				});
 			} else {
