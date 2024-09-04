14a42afe6bf39ae6a343c48300c53defab9a0a8b
mhli1260 <118139796+mhli1260@users.noreply.github.com>
2024-09-03 21:12:09 -0400

    Refactor digest.js by creating a separate errorCheck function

--- public/src/admin/manage/digest.js
+++ public/src/admin/manage/digest.js
@@ -1,9 +1,15 @@
 'use strict';
 
+function errorCheck (err) {
+	if (err) {
+		return alerts.error(err);
+	}
 
+	alerts.success('[[admin/manage/digest:resent-single]]');
+}
 define('admin/manage/digest', ['bootbox', 'alerts'], function (bootbox, alerts) {
 	const Digest = {};
-
+	
 	Digest.init = function () {
 		$('.digest').on('click', '[data-action]', function () {
 			const action = this.getAttribute('data-action');
@@ -13,23 +19,11 @@ define('admin/manage/digest', ['bootbox', 'alerts'], function (bootbox, alerts) 
 				const interval = action.slice(7);
 				bootbox.confirm('[[admin/manage/digest:resend-all-confirm]]', function (ok) {
 					if (ok) {
-						Digest.send(action, undefined, function (err) {
-							if (err) {
-								return alerts.error(err);
-							}
-
-							alerts.success('[[admin/manage/digest:resent-' + interval + ']]');
-						});
+						Digest.send(action, undefined, errorCheck(err);
 					}
 				});
 			} else {
-				Digest.send(action, uid, function (err) {
-					if (err) {
-						return alerts.error(err);
-					}
-
-					alerts.success('[[admin/manage/digest:resent-single]]');
-				});
+				Digest.send(action, uid, );
 			}
 		});
 	};
